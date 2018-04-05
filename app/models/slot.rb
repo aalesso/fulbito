57e0dcc1f4a93a3a2ec48550e47d29388834301e
Raghu Betina <raghu@firstdraft.com>
2018-04-05 20:54:36 +0000

    Direct association between users and slots

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,6 +1,9 @@
 class Slot < ApplicationRecord
   # Direct associations
 
+  belongs_to :users,
+             :class_name => "User"
+
   belongs_to :courts,
              :class_name => "Court",
              :counter_cache => true
